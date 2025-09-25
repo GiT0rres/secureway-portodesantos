5215d3beca5e9d7ec14993344a704f0d9a47a568
Giovanna Torres <giovanna.alvesmtorres@gmail.com>
2025-09-24 21:55:54 -0300

    tentando por a imagem

--- SecureWay/Código/Front-end/Tela Carregamento/script.js
+++ SecureWay/Código/Front-end/Tela Carregamento/script.js
@@ -1,11 +1,9 @@
-// Simula o carregamento do site
 window.addEventListener("load", () => {
   setTimeout(() => {
     document.getElementById("loading").style.display = "none";
     document.getElementById("content").style.display = "block";
-  }, 3000); // tempo em ms (3 segundos)
+  }, 5000);
 });
-// Adiciona um efeito de fade-in ao conteúdo após o carregamento
 document.addEventListener("DOMContentLoaded", () => {
   const content = document.getElementById("content");
   content.classList.add("fade-in");
