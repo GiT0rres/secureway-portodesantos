a1b535bdd1aaf800d884e92862c77d56b6a85421
GiT0rres <giovanna.alvesmtorres@gmail.com>
2025-10-04 01:16:42 -0300

    aparentemente, aes feito para não dar erro e dependencias colocadas de forma devida.

---
+++ SecureWay App/react-native.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  dependencies: {
+    'react-native-reanimated': {
+      platforms: {
+        android: {
+          newArchEnabled: true,
+        },
+      },
+    },
+    'react-native-worklets': {
+      platforms: {
+        android: {
+          newArchEnabled: true,
+        },
+      },
+    },
+  },
+};
